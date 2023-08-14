37e3da2b9bb2723663ff437649a9dcb77d6169b9
Younghun1213 <138798686+Younghun1213@users.noreply.github.com>
2023-08-14 10:56:39 +0900

    Add files via upload

---
+++ project/src/main/java/config/DbConfig.java
@@ -0,0 +1,41 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+@Profile("!real")//real아닌 프로필
+@Configuration
+public class DbConfig {
+    @Value("${db.class.name}")
+    private String driverClassName;
+    @Value("${db.url}")
+    private String dbUrl;
+    @Value("${db.username}")
+    private String userName;
+    @Value("${db.password}")
+    private String password;
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName(driverClassName);
+        ds.setUrl(dbUrl);
+        ds.setUsername(userName);
+        ds.setPassword(password);
+
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true);
+        ds.setMinEvictableIdleTimeMillis(1000 * 30);
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 3);
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
